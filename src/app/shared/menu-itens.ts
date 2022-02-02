7ad69882cec65dad488b8a44637946f2bb0c0ecc
boris <badmatss@gmail.com>
2022-02-01 23:47:21 -0300

    nova fonte

--- src/app/shared/menu-itens.ts
+++ src/app/shared/menu-itens.ts
@@ -1,24 +1,28 @@
 const MENUITENS = [
     {
         nome: 'Opção 1', 
+        tamNome: '12px',
         nomeIcone: 'mdi mdi-24px mdi-airplane', 
         tamIcone: '1.6rem',
         rota: 'home'                       
     },
     {
         nome: 'Opção 2', 
+        tamNome: '12px',
         nomeIcone: 'pi pi-home',
         tamIcone: '1.6rem',
         rota: 'home'
     },
     {
         nome: 'Opção 3', 
+        tamNome: '12px',
         nomeIcone: 'mdi mdi-24px mdi-cake',
         tamIcone: '1.6rem',
         rota: 'home'
     },
     {
         nome: 'Opção 4', 
+        tamNome: '12px',
         nomeIcone: 'mdi mdi-24px mdi-car-emergency',
         tamIcone: '1.6rem',
         rota: 'home'
